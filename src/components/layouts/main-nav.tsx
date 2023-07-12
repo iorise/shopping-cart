b177ed1edfc1d9460f5050ac64fa46f275677161
iorise <riofirmansyah1805@gmail.com>
2023-07-12 20:10:04 +0700

    add navbar

---
+++ src/components/layouts/main-nav.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+
+import { MainNavItem } from "@/types";
+import { Icons } from "@/components/icons";
+import { siteConfig } from "@/config/site-config";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+interface MainNavProps {
+  mainNavItems?: MainNavItem[];
+}
+
+export function MainNav({ mainNavItems }: MainNavProps) {
+  return (
+    <div className="hidden gap-6 lg:flex">
+      <Link
+        aria-label="home"
+        href="/"
+        className="hidden items-center space-x-2 lg:flex"
+      >
+        <Icons.shoppingCart className="h-6 w-6" aria-hidden="true" />
+        <span className="hidden font-bold lg:inline-block text-lg md:text-lg">
+          {siteConfig.name}
+        </span>
+      </Link>
+
+      <NavigationMenu>
+        <NavigationMenuList>
+          {mainNavItems?.map((item) => item.href && (
+            <NavigationMenuItem key={item.title}>
+              <Link href={item.href} legacyBehavior passHref>
+                <NavigationMenuLink
+                  className={cn(navigationMenuTriggerStyle(), "text-base")}
+                >
+                  {item.title}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
---
+++ src/components/layouts/mobile-nav.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import * as React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+import { MainNavItem, SidebarNavItem } from "@/types";
+import { siteConfig } from "@/config/site-config";
+
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { ScrollArea } from "../ui/scroll-area";
+import { Button } from "../ui/button";
+import { Icons } from "../icons";
+import { Accordion, AccordionItem, AccordionTrigger } from "../ui/accordion";
+
+interface MobileNavProps {
+  mainNavItems?: MainNavItem[];
+}
+
+export function MobileNav({ mainNavItems }: MobileNavProps) {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden"
+        >
+          <Icons.menu className="h-6 w-6" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="pl-1 pr-0">
+        <div className="px-7">
+          <Link
+            aria-label="home"
+            href="/"
+            className="flex items-center"
+            onClick={() => setIsOpen(false)}
+          >
+            <Icons.shoppingCart className="mr-2 h-4 w-4" aria-hidden="true" />
+            <span className="font-bold">{siteConfig.name}</span>
+          </Link>
+        </div>
+        <ScrollArea className="my-6 h-[calc(100vh-8rem)] pb-10 pl-6">
+          <div className="pl-1 pr-7">
+            {mainNavItems?.map((item, index) => item.href && (
+              <React.Fragment>
+                <div
+                  className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline hover:cursor-pointer"
+                  key={index}
+                >
+                  <Link href={item.href}>{item.title}</Link>
+                </div>
+              </React.Fragment>
+            ))}
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+}
