7eb1b6efd88c0b1b7b3facee74bf9b69a47222a1
iorise <riofirmansyah1805@gmail.com>
2023-07-13 11:03:37 +0700

    add products config

---
+++ src/config/products.ts
@@ -0,0 +1,54 @@
+export const sortOptions = [
+  {
+    label: "Date: Old to new",
+    value: "createdAt.asc",
+  },
+  {
+    label: "Date: New to old",
+    value: "createdAt.desc",
+  },
+  {
+    label: "Price: Low to high",
+    value: "price.asc",
+  },
+  {
+    label: "Price: High to low",
+    value: "price.desc",
+  },
+  {
+    label: "Alphabetical: A to Z",
+    value: "name.asc",
+  },
+  {
+    label: "Alphabetical: Z to A",
+    value: "name.desc",
+  },
+];
+
+export const productCategories = [
+  {
+    title: "Products",
+    href: "/products",
+    description: "Find a wide selection of products that are attractive and according to your needs."
+  },
+  {
+    title: "Electronics",
+    href: "/category/electronics",
+    description: ""
+  },
+  {
+    title: "Jewelry",
+    href: "/category/jewelry",
+    description: ""
+  },
+  {
+    title: "Men's Clothing",
+    href: "/category/mens-clothing",
+    description: ""
+  },
+  {
+    title: "Women's Clothing",
+    href: "/category/womens-clothing",
+    description: ""
+  },
+];
