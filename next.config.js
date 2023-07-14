9cc7725371e8eaf190c219201e3272b9082c83b9
iorise <riofirmansyah1805@gmail.com>
2023-07-14 22:23:33 +0700

    update package.json

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const nextConfig = {};
 
 module.exports = {
   images: {
-    domains: ["images.unsplash.com", "fakestoreapi.com"]
+    domains: ["images.unsplash.com", "fakestoreapi.com"],
   },
 };
