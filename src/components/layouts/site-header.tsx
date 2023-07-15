2693b04ce8f82c5ccee05c0055abb2cd6f469b80
iorise <riofirmansyah1805@gmail.com>
2023-07-15 14:11:18 +0700

    navbar final touch

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import React from "react";
 
 import { siteConfig } from "@/config/site-config";
 
 import { MainNav } from "@/components/layouts/main-nav";
 import { MobileNav } from "@/components/layouts/mobile-nav";
+import CartSheet from "../cart/cart-sheet";
+import { Combobox } from "../combobox";
 
 const SiteHeader = () => {
   return (
@@ -11,6 +15,12 @@ const SiteHeader = () => {
       <div className="container flex h-16 items-center">
         <MainNav mainNavItems={siteConfig.mainNav} />
         <MobileNav mainNavItems={siteConfig.mainNav} />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-2">
+            <Combobox/>
+            <CartSheet/>
+          </nav>
+        </div>
       </div>
     </header>
   );
