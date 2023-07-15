13968e5e142427dfbb27b6900b35589aa2a3d27c
iorise <riofirmansyah1805@gmail.com>
2023-07-15 15:20:14 +0700

    clean code

--- src/app/(lobby)/products/layout.tsx
+++ src/app/(lobby)/products/layout.tsx
@@ -1,16 +1,13 @@
 interface ProductsLayoutProps {
     children: React.ReactNode
-    modal: React.ReactNode
   }
   
   export default function ProductsLayout({
     children,
-    modal,
   }: ProductsLayoutProps) {
     return (
       <>
         {children}
-        {modal}
       </>
     )
   }
